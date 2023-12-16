f82042bfd52b65973078fb31d7ce0e4ef3a17534
eemmyk <eemmyk-8@student.ltu.se>
2023-12-16 12:25:56 +0100

    OAD equations nice

--- FinalProject.m
+++ FinalProject.m
@@ -1,3 +1,8 @@
+%% Shape-Based Approach to Low-Thrust Rendezvous Trajectory Design
+
+clear;
+close all;
+
 t = linspace(0, 100, 1000);
 
 N = 2;
@@ -8,13 +13,35 @@ theta_tilde = 0;
 
 %The total transfer angle is represented by:
 theta_f = 2*pi * N + theta_tilde;
-theta = linspace(theta_0, theta_f, 1000);
+%theta = linspace(theta_0, theta_f, 1000);
 
-r1 = 100 * 10^6;
-r2 = 150 * 10^6;
+mju_Earth = 3.986004418*10^14;
+mju_Sun = 1.32712440018*10^20;
 
+mju = mju_Earth;
+
+%initial and target orbit parameters
+r1 = 6800*1000;%100 * 10^9;
+e1 = 0.02;
+E1 = theta_0;
+gamma1 = asin(e1 * sin(E1) / (1-e1*cos(E1)));
+a1 = r1 / (1 - e1 * cos(E1));
+%theta2_dot = d/dt acos((1 - r/a)/e); -->
+v1 = sqrt(2*mju / r1 - mju/a1)
+theta1_dot = v1/(a1*sqrt(-(r1^2/a1^2) + 2*r1/a1 + e1^2 - 1))
+
+r2 = 15000*1000;%150 * 10^9;
+e2 = 0.01;
+E2 = theta_f;
+gamma2 = asin(e2 * sin(E2) / (1-e2*cos(E2)));
+a2 = r2 / (1 - e2* cos(E2));
+%theta2_dot = d/dt acos((1 - r/a)/e); -->
+v2 = sqrt(2*mju / r2 - mju/a2)
+theta2_dot = v2/(a2*sqrt(-(r2^2/a2^2) + 2*r2/a2 + e2^2 -1))
+
+%%
 % The radius of the orbit can be represented as:
-% r(theta) 1/(a + b*theta + c*theta^2 + d*theta^3 + e*theta^4 + f*theta^5)
+% r(theta) 1/(a + b*theta + c*theta.^2 + d*theta.^3 + e*theta.^4 + f*theta.^5)
 
 %theta = theta_0 = 0 yields:
 %r1 = 1/a
@@ -23,14 +50,14 @@ r2 = 150 * 10^6;
 %r2 = 1/(a + b*theta_f + c*theta_f^2 + d*theta_f^3 + e*theta_f^4 + f*theta_f^5);
 
 % The time derivative of the radius equation yields
-% r_dot = -r.^2 * (b + 2*c*theta + 3*d*theta^2 + 4*e*theta^3 + 5*f*theta^4)*theta_dot
+% r_dot = -r.^2 * (b + 2*c*theta + 3*d*theta.^2 + 4*e*theta.^3 + 5*f*theta.^4)*theta_dot
 % From this we can get the flight-path angle gamma may be found
-% tan(gamma) = r_dot / (r * theta_dot) = -r * (b + 2*c*theta + 3*d*theta^2 + 4*e*theta^3 + 5*f*theta^4)
+% tan(gamma) = r_dot / (r * theta_dot) = -r * (b + 2*c*theta + 3*d*theta.^2 + 4*e*theta.^3 + 5*f*theta.^4)
 
 
 %The first and final flight path angles gamma1 and gamma2 can be solved from this equation:
 %tan(gamma1) = -r1 * b
-%tan(gamma2) = -r2 * (b + 2*c*theta + 3*d*theta^2 + 4*e*theta^3 + 5*f*theta^4)
+%tan(gamma2) = -r2 * (b + 2*c*theta + 3*d*theta.^2 + 4*e*theta.^3 + 5*f*theta.^4)
 
 %Additionally the polynomial must satisfy the equations of motion
 %r_dot_dot = r*theta_dot^2 + mju/r^2 = Ta * sin(alpha)
@@ -42,11 +69,18 @@ r2 = 150 * 10^6;
 
 %A LOT OF STUFF HERE:
 
-syms d;
+syms a b c d f theta;
+
+
+theta1_dot = sqrt(mju/r1^4) / ((1/r1) + 2*c);
+theta2_dot = sqrt(mju/r2^4) / ((1/r2) + 2*c + 6*d*theta_f + 12*e*theta_f^2 + 20 * f*theta_f^3);
+
+%gamma1 = atan(-r1 * b);
+%gamma2 = atan(-r2 * (b + 2*c*theta_f + 3*d*theta_f^2 + 4*e*theta_f^3 + 5*f*theta_f^4));
 
 a = 1/r1;
 b = -tan(gamma1) / r1;
-c = 1/(2*r1) * (mju / (r1^3 * theta_dot_1) - 1);
+c = 1/(2*r1) * (mju / (r1^3 * theta1_dot) - 1);
 
 efg_Mat_1 = [30*theta_f^2  -10*theta_f^3  theta_f^4;
             -48*theta_f     18*theta_f^2 -2*theta_f^3; 
@@ -64,10 +98,15 @@ g = efg(3);
 
 %r2 = 1/(a + b*theta_f + c*theta_f^2 + d*theta_f^3 + e*theta_f^4 + f*theta_f^5);
 
-f_Theta_a = sqrt((mju/r^4)) / (1/r + 2*c + 6*d*theta + 2*e*theta^2 + 20*f*theta^3 + 30*g*theta^4);
-f_T_a = -mju / (2 * r^3 * cos(gamma)) * (6*d + 24*e*theta + 60*f*theta^2 + 120*g*theta^3 - tan(gamma)/r) / (1/r + 2*c + 6*d*theta + 2*e*theta^2 + 20*f*theta^3 + 30*g*theta^4)^2;
+r = 1 ./ (a + b*theta + c*theta.^2 + d*theta.^3 + e*theta.^4 + f*theta.^5);
+gamma = atan(-r * (b + 2*c*theta + 3*d*theta^2 + 4*e*theta^3 + 5*f*theta^4));
 
-costFunction = @(theta) f_T_a / f_Theta_dot;
+f_Theta_dot = sqrt((mju./r.^4)) ./ (1./r + 2*c + 6*d*theta + 2*e*theta.^2 + 20*f*theta.^3 + 30*g*theta.^4);
+f_T_a = -mju ./ (2 * r.^3 * cos(gamma)) * (6*d + 24*e*theta + 60*f*theta.^2 + 120*g*theta.^3 - tan(gamma)./r) ./ (1./r + 2*c + 6*d*theta + 2*e*theta.^2 + 20*f*theta.^3 + 30*g*theta.^4).^2;
+
+megaFunction = f_T_a / f_Theta_dot;
+
+costFunction = @(theta) megaFunction;
 
 deltaV = integral(costFunction, theta_0, theta_f);
 
