aea9203799fdd2152cc606feb49d61f33fdd8f11
Tom Causer <TomMCauser@gmail.com>
2023-12-19 22:28:10 +0100

    Lord help me.

--- FinalProjectTom.m
+++ FinalProjectTom.m
@@ -12,20 +12,31 @@ G = 6.6743e-11; % gravitational parameter Nm^2/kg^2
 M_earth = 5.972e+24; % mass of Earth, kg
 mu = G*M_earth;
 
-r1 = orbitalAltitude_inner + earthRadius;
-r2 = orbitalAltitude_outer + earthRadius;
-
 %..........................................................................
 % Orbital Parameters
 %..........................................................................
 
-N = 1; % number of loops
+% Inner Orbit
+
+semiMajor_inner = orbitalAltitude_inner + earthRadius;
+orbitalPeriod_inner = 2*pi*sqrt((semiMajor_inner^3)/mu);
+r1 = semiMajor_inner;
+e1 = 0.2;
+theta_dot1 = sqrt((semiMajor_inner^3)/mu);
+
+% Outer Orbit
+
+semiMajor_outer = orbitalAltitude_outer + earthRadius;
+orbitalPeriod_outer = 2*pi*sqrt((semiMajor_outer^3)/mu);
+r2 = semiMajor_outer;
+e2 = 0.3;
+theta_dot2 = sqrt((semiMajor_outer^3)/mu);
+
+% Transfer Path
+
+N = 1; % number of transfer loops
 theta_0 = 0; % r1 angle
 theta_tilde = pi/2; % angle between r1 and r2
-
-semiMajor = (r1+r2)/2;
-orbitalPeriod = 2*pi*((semiMajor^3)/mu);
-
 theta_f = 2*pi * N + theta_tilde;
 theta = linspace(theta_0, theta_f, 1e2);
 
@@ -35,10 +46,8 @@ transferTime = 23*60*60; % seconds
 % The "I don't get how these are supposed to be defined" section
 %..........................................................................
 
-theta_dot = theta_f/orbitalPeriod; % rads/s
 gamma1 = pi/20;
 gamma2 = -pi/20;
-d = 0;
 
 %..........................................................................
 % Coefficients Definitions
@@ -48,7 +57,7 @@ d = 0;
 
 a = 1/r1;
 b = -tan(gamma1)/r1;
-c = (1/2*r1)*((mu/((r1^3)*theta_dot^2)) - 1);
+c = (1/(2*r1))*((mu/((r1^3)*theta_dot1^2)) - 1);
 
 % Next, e, f, and g are found through use of the syms package
 
@@ -60,7 +69,7 @@ efgMatrix1 = [30*theta_f_^2, -10*theta_f_^3, theta_f_^4;
 efgMatrix2 = [(1/r2_) - (a_ + b_*theta_f_ + c_*theta_f_^2 + d_*theta_f_^3);
               -(tan(gamma2_)/r2_) - (b_ + 2*c_*theta_f_ + 3*d_*theta_f_^2);
               (mu_/((r2_^4)*theta_dot_^2)) - ((1/r2_) + 2*c_ + 6*d_*theta_f_)];
-efgSolution_sym = (1/(theta_f_^6))*efgMatrix1*efgMatrix2;
+efgSolution_sym = (1/(2*theta_f_^6))*efgMatrix1*efgMatrix2;
 
 % efgSolution = subs(efgSolution_sym, [theta_f_, theta_dot_, r2_, a_, b_, c_, d_, gamma2_, mu_], ...
 %                    [theta_f, theta_dot, r2, a, b, c, d, gamma2, mu]);
@@ -75,43 +84,7 @@ efgSolution_sym = (1/(theta_f_^6))*efgMatrix1*efgMatrix2;
 % Finding the transfer time calculated by the constants
 %..........................................................................
 
-% This section is deprecated because it was attempting analytical
-% integration
-
-% rFunc = 1 ./ (a_ + b_*theta_ + c_*theta_.^2 + d_*theta_.^3 + e_*theta_.^4 ...
-%               + f_*theta_.^5 + g_*theta_.^6);
-% 
-% tfFunction = sqrt(((rFunc^4)/mu_)*((1/rFunc) + 2*c_ + 6*d_*theta_ + 12*e_*theta_^2 ...
-%                   + 20*f_*theta_^3 + 30*g_*theta_^4));
-% 
-% t_f_ = int(tfFunction, theta_, [0, tf_]);
-
-% rFunc = 1 ./ (a + b*thetaInt + c*thetaInt.^2 + d*thetaInt.^3 + e*thetaInt.^4 ...
-%               + f*thetaInt.^5 + g*thetaInt.^6);
-
-% debugTest = real(sqrt(((rFunc.^4)/mu).*((1./rFunc) + 2*c + 6*d*theta + 12*e*theta.^2 ...
-%                    + 20*f*theta.^3 + 30*g*theta.^4)));
-
-% integrateTransferTime = @(thetaInt) real(sqrt((((1 ./ (a + b*thetaInt + c*thetaInt.^2 + d*thetaInt.^3 + e*thetaInt.^4 ...
-%               + f*thetaInt.^5 + g*thetaInt.^6)).^4)/mu).*((1./(1 ./ (a + b*thetaInt + c*thetaInt.^2 + d*thetaInt.^3 + e*thetaInt.^4 ...
-%               + f*thetaInt.^5 + g*thetaInt.^6))) + 2*c + 6*d*thetaInt + 12*e*thetaInt.^2 ...
-%                   + 20*f*thetaInt.^3 + 30*g*thetaInt.^4)));
-% 
-% tf_integralValue = integral(integrateTransferTime, 0, theta_f);
-% 
-% dFinder = fzero(integrateTransferTime - transferTime, 0);
-
-bestGuess = 1e10;
-
-firstBound = 1e20;
-boundStep = 1e20;
-secondBound = 1e25;
-
-% dValue = fzero(@dOptimiser, 0);
-dValue = fzero(@dOptimiser, 1e-15);
-
-% 
-% dOptimiser(1e20)
+dValue = fzero(@dOptimiser, 0);
 
 function dOptimised = dOptimiser(dGuess)
     earthRadius = 6.371e6; % m
@@ -121,22 +94,37 @@ function dOptimised = dOptimiser(dGuess)
     M_earth = 5.972e+24; % mass of Earth, kg
     mu = G*M_earth;
     
-    r1 = orbitalAltitude_inner + earthRadius;
-    r2 = orbitalAltitude_outer + earthRadius;
-    N = 1; % number of loops
+    % Inner Orbit
+    
+    semiMajor_inner = orbitalAltitude_inner + earthRadius;
+    orbitalPeriod_inner = 2*pi*sqrt((semiMajor_inner^3)/mu);
+    r1 = semiMajor_inner;
+    e1 = 0.2;
+    theta_dot1 = sqrt((semiMajor_inner^3)/mu);
+    
+    % Outer Orbit
+    
+    semiMajor_outer = orbitalAltitude_outer + earthRadius;
+    orbitalPeriod_outer = 2*pi*sqrt((semiMajor_outer^3)/mu);
+    r2 = semiMajor_outer;
+    e2 = 0.3;
+    theta_dot2 = sqrt((semiMajor_outer^3)/mu);
+    
+    % Transfer Path
+    
+    N = 1; % number of transfer loops
     theta_0 = 0; % r1 angle
     theta_tilde = pi/2; % angle between r1 and r2
-    
-    semiMajor = (r1+r2)/2;
-    orbitalPeriod = 2*pi*((semiMajor^3)/mu);
-
     theta_f = 2*pi * N + theta_tilde;
-    theta_dot = theta_f/orbitalPeriod; % rads/s
+    theta = linspace(theta_0, theta_f, 1e2);
+    
+    transferTime = 23*60*60; % seconds
+
     gamma1 = pi/20;
     gamma2 = -pi/20;
     a = 1/r1;
     b = -tan(gamma1)/r1;
-    c = (1/2*r1)*((mu/((r1^3)*theta_dot^2)) - 1);
+    c = (1/(2*r1))*((mu/((r1^3)*theta_dot1^2)) - 1);
     % Definitions since MATLAB hates global variables
     syms theta_f_ theta_dot_ r2_ a_ b_ c_ d_ e_ f_ g_ gamma2_ mu_ r_ theta_ tf_
     symbolicInputs = [theta_f_, theta_dot_, r2_, a_, b_, c_, d_, gamma2_, mu_];
@@ -147,9 +135,8 @@ function dOptimised = dOptimiser(dGuess)
     efgMatrix2 = [(1/r2_) - (a_ + b_*theta_f_ + c_*theta_f_^2 + d_*theta_f_^3);
                   -(tan(gamma2_)/r2_) - (b_ + 2*c_*theta_f_ + 3*d_*theta_f_^2);
                   (mu_/((r2_^4)*theta_dot_^2)) - ((1/r2_) + 2*c_ + 6*d_*theta_f_)];
-    efgSolution_sym = (1/(theta_f_^6))*efgMatrix1*efgMatrix2;
-    transferTime = 23*60*60; % seconds
-    efgSolution = subs(efgSolution_sym, symbolicInputs, [theta_f, theta_dot, r2, a, b, c, dGuess, gamma2, mu]);
+    efgSolution_sym = (1/(2*theta_f_^6))*efgMatrix1*efgMatrix2;
+    efgSolution = subs(efgSolution_sym, symbolicInputs, [theta_f, theta_dot2, r2, a, b, c, dGuess, gamma2, mu]);
     efgSolution = double(efgSolution);
     e = efgSolution(1);
     f = efgSolution(2);
@@ -159,29 +146,8 @@ function dOptimised = dOptimiser(dGuess)
               + f*thetaInt.^5 + g*thetaInt.^6))) + 2*c + 6*dGuess*thetaInt + 12*e*thetaInt.^2 ...
                   + 20*f*thetaInt.^3 + 30*g*thetaInt.^4)));
     tf_integralValue = integral(integrateTransferTime, 0, theta_f);
-    dOptimised = abs(tf_integralValue - transferTime);
+    dOptimised = transferTime - tf_integralValue;
 end
-
-
-% for dGuess = firstBound:boundStep:secondBound
-%     efgSolution = subs(efgSolution_sym, [theta_f_, theta_dot_, r2_, a_, b_, c_, d_, gamma2_, mu_], ...
-%                    [theta_f, theta_dot, r2, a, b, c, dGuess, gamma2, mu]);
-%     efgSolution = double(efgSolution);
-%     e = efgSolution(1);
-%     f = efgSolution(2);
-%     g = efgSolution(3);
-%     integrateTransferTime = @(thetaInt) abs(sqrt((((1 ./ (a + b*thetaInt + c*thetaInt.^2 + dGuess*thetaInt.^3 + e*thetaInt.^4 ...
-%               + f*thetaInt.^5 + g*thetaInt.^6)).^4)/mu).*((1./(1 ./ (a + b*thetaInt + c*thetaInt.^2 + dGuess*thetaInt.^3 + e*thetaInt.^4 ...
-%               + f*thetaInt.^5 + g*thetaInt.^6))) + 2*c + 6*dGuess*thetaInt + 12*e*thetaInt.^2 ...
-%                   + 20*f*thetaInt.^3 + 30*g*thetaInt.^4)));
-%     tf_integralValue = integral(integrateTransferTime, 0, theta_f);
-%     minimisationValue = abs(tf_integralValue - transferTime);
-%     if minimisationValue <= bestGuess
-%         bestGuess = minimisationValue;
-%         bestdGuess = dGuess;
-%     end
-%     fprintf('Running: %2f completed.\n', (((dGuess - firstBound)/(secondBound-firstBound)))*100)
-% end
 
 
 
