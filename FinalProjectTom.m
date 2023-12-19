75653027a6a50aeaaf0a1adbf9788bf6e96d9243
Tom Causer <TomMCauser@gmail.com>
2023-12-19 17:26:03 +0100

    Manually changing d value to find.

--- FinalProjectTom.m
+++ FinalProjectTom.m
@@ -1,3 +1,6 @@
+clc
+clear all
+
 %..........................................................................
 % Constants Section
 %..........................................................................
@@ -24,7 +27,7 @@ semiMajor = (r1+r2)/2;
 orbitalPeriod = 2*pi*((semiMajor^3)/mu);
 
 theta_f = 2*pi * N + theta_tilde;
-theta = linspace(theta_0, theta_f, 1e6);
+theta = linspace(theta_0, theta_f, 1e2);
 
 transferTime = 23*60*60; % seconds
 
@@ -35,7 +38,7 @@ transferTime = 23*60*60; % seconds
 theta_dot = theta_f/orbitalPeriod; % rads/s
 gamma1 = pi/20;
 gamma2 = -pi/20;
-d = 1e+10;
+d = 0;
 
 %..........................................................................
 % Coefficients Definitions
@@ -59,26 +62,74 @@ efgMatrix2 = [(1/r2_) - (a_ + b_*theta_f_ + c_*theta_f_^2 + d_*theta_f_^3);
               (mu_/((r2_^4)*theta_dot_^2)) - ((1/r2_) + 2*c_ + 6*d_*theta_f_)];
 efgSolution_sym = (1/(theta_f_^6))*efgMatrix1*efgMatrix2;
 
-efgSolution_sym = subs(efgSolution_sym, [theta_f_, theta_dot_, r2_, a_, b_, c_, d_, gamma2_, mu_], ...
-                   [theta_f, theta_dot, r2, a, b, c, d, gamma2, mu]);
-
-efgSolution = double(efgSolution_sym);
-
-e = efgSolution(1);
-f = efgSolution(2);
-g = efgSolution(3);
+% efgSolution = subs(efgSolution_sym, [theta_f_, theta_dot_, r2_, a_, b_, c_, d_, gamma2_, mu_], ...
+%                    [theta_f, theta_dot, r2, a, b, c, d, gamma2, mu]);
+% 
+% efgSolution = double(efgSolution);
+% 
+% e = efgSolution(1);
+% f = efgSolution(2);
+% g = efgSolution(3);
 
 %..........................................................................
 % Finding the transfer time calculated by the constants
 %..........................................................................
 
-rFunc = 1 ./ (a_ + b_*theta_ + c_*theta_.^2 + d_*theta_.^3 + e_*theta_.^4 ...
-              + f_*theta_.^5 + g_*theta_.^6);
+% This section is deprecated because it was attempting analytical
+% integration
 
-tfFunction = sqrt(((rFunc^4)/mu_)*((1/rFunc) + 2*c_ + 6*d_*theta_ + 12*e_*theta_^2 ...
-                  + 20*f_*theta_^3 + 30*g_*theta_^4));
+% rFunc = 1 ./ (a_ + b_*theta_ + c_*theta_.^2 + d_*theta_.^3 + e_*theta_.^4 ...
+%               + f_*theta_.^5 + g_*theta_.^6);
+% 
+% tfFunction = sqrt(((rFunc^4)/mu_)*((1/rFunc) + 2*c_ + 6*d_*theta_ + 12*e_*theta_^2 ...
+%                   + 20*f_*theta_^3 + 30*g_*theta_^4));
+% 
+% t_f_ = int(tfFunction, theta_, [0, tf_]);
 
-t_f_ = int(tfFunction, theta_, [0, tf_]);
+% rFunc = 1 ./ (a + b*thetaInt + c*thetaInt.^2 + d*thetaInt.^3 + e*thetaInt.^4 ...
+%               + f*thetaInt.^5 + g*thetaInt.^6);
+
+% debugTest = real(sqrt(((rFunc.^4)/mu).*((1./rFunc) + 2*c + 6*d*theta + 12*e*theta.^2 ...
+%                    + 20*f*theta.^3 + 30*g*theta.^4)));
+
+% integrateTransferTime = @(thetaInt) real(sqrt((((1 ./ (a + b*thetaInt + c*thetaInt.^2 + d*thetaInt.^3 + e*thetaInt.^4 ...
+%               + f*thetaInt.^5 + g*thetaInt.^6)).^4)/mu).*((1./(1 ./ (a + b*thetaInt + c*thetaInt.^2 + d*thetaInt.^3 + e*thetaInt.^4 ...
+%               + f*thetaInt.^5 + g*thetaInt.^6))) + 2*c + 6*d*thetaInt + 12*e*thetaInt.^2 ...
+%                   + 20*f*thetaInt.^3 + 30*g*thetaInt.^4)));
+% 
+% tf_integralValue = integral(integrateTransferTime, 0, theta_f);
+% 
+% dFinder = fzero(integrateTransferTime - transferTime, 0);
+
+bestGuess = 1e10;
+
+firstBound = 1e20;
+boundStep = 1e20;
+secondBound = 1e25;
+
+
+for dGuess = firstBound:boundStep:secondBound
+    efgSolution = subs(efgSolution_sym, [theta_f_, theta_dot_, r2_, a_, b_, c_, d_, gamma2_, mu_], ...
+                   [theta_f, theta_dot, r2, a, b, c, dGuess, gamma2, mu]);
+    efgSolution = double(efgSolution);
+    e = efgSolution(1);
+    f = efgSolution(2);
+    g = efgSolution(3);
+    integrateTransferTime = @(thetaInt) abs(sqrt((((1 ./ (a + b*thetaInt + c*thetaInt.^2 + dGuess*thetaInt.^3 + e*thetaInt.^4 ...
+              + f*thetaInt.^5 + g*thetaInt.^6)).^4)/mu).*((1./(1 ./ (a + b*thetaInt + c*thetaInt.^2 + dGuess*thetaInt.^3 + e*thetaInt.^4 ...
+              + f*thetaInt.^5 + g*thetaInt.^6))) + 2*c + 6*dGuess*thetaInt + 12*e*thetaInt.^2 ...
+                  + 20*f*thetaInt.^3 + 30*g*thetaInt.^4)));
+    tf_integralValue = integral(integrateTransferTime, 0, theta_f);
+    minimisationValue = abs(tf_integralValue - transferTime);
+    if minimisationValue <= bestGuess
+        bestGuess = minimisationValue;
+        bestdGuess = dGuess;
+    end
+    fprintf('Running: %2f completed.\n', (((dGuess - firstBound)/(secondBound-firstBound)))*100)
+end
+
+
+
 
 %..........................................................................
 % Plotting Section
